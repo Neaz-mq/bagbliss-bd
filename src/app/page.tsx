b4e56cb0ad9c9835abf8c58083c103427f60310f
Neaz-mq <neazmorshed666@gmail.com>
2026-06-19 09:45:14 +0600

    featuredproduct updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import HeroSection from '@/components/home/HeroSection'
 import CategoryStrip from '@/components/home/CategoryStrip'
 import FeaturedProducts from '@/components/home/FeaturedProducts'
-import FlashSale from '@/components/home/FlashSale'
 
 export default function Home() {
   return (
@@ -9,7 +8,7 @@ export default function Home() {
       <HeroSection />
       <CategoryStrip />
       <FeaturedProducts />
-      <FlashSale />
+     
     </main>
   )
 }
